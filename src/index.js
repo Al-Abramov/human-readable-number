c64376a8797b2988c78bac108709caf4181ee6bd
Alex <x3m.f@mail.ru>
2021-03-03 18:12:21 +0400

    feat: add implement function for task Human Readable Number

--- src/index.js
+++ src/index.js
@@ -35,38 +35,41 @@ module.exports = function toReadable(number) {
         "eighty",
         "ninety",
     ];
-    if (number < 10) {
+
+    if(number < 10) {
         return dg[number];
     }
-    if (number > 9 && number < 20) {
-        let res = number.toString().split("").splice(1, 1);
-        return tn[res];
+    if(number > 9 && number < 20) {
+       let tens = number.toString().slice(1, 2);
+        return tn[tens];
     }
-    if (number >= 20 && number < 100) {
-        let res1 = number.toString().split("");
-        let res2 = number.toString().split("").splice(1);
-        if (res2[0] === "0") {
-            return tw[res1[0]];
-        } else {
-            return `${tw[res1[0]]} ${dg[res2]}`;
+    if(number >= 20 && number < 100) {
+        let firstNum = number.toString().slice(0, 1);
+        let secondNum= number.toString().slice(1, 2);
+        if(secondNum === '0') {
+            return tw[firstNum];
         }
+        return `${tw[firstNum]} ${dg[secondNum]}`;
     }
-    if(number >=100 && number < 1000) {
-        let firstNum = number.toString().split('').splice(0,1);
-        let secNum = number.toString().split('').splice(1,1,1)
-        let lastNum = number.toString().split('').splice(2,1);
-        if(secNum[0] === '0' && lastNum[0] === '0'){
+    if(number >= 100 && number < 1000) {
+        let zero = number.toString().split('');
+        let firstNum = number.toString().slice(0, 1);
+        let secondNum= number.toString().slice(1, 2);
+        let therdNum = number.toString().slice(-1);
+
+        if(zero[1] === '0' && zero[2] === '0') {
             return dg[firstNum] + ' hundred';
         }
-        if(secNum[0] === '0') {
-            return dg[firstNum] + ' hundred ' + dg[lastNum];
+        if(zero[1] === '0') {
+           return dg[firstNum] + ' hundred ' + dg[therdNum];
         }
-        if(secNum[0] === '1') {
-            return dg[firstNum] + ' hundred ' + tn[lastNum];
-        }if(lastNum[0] === '0'){
-            return dg[firstNum] + ' hundred ' + tw[secNum]
-        }else {
-        return dg[firstNum] + ' hundred ' + `${tw[secNum]} ${dg[lastNum]}`;
+        if(zero[1] === '1') {
+            return dg[firstNum] + ' hundred ' + tn[therdNum];
         }
+         if(zero[2] === '0') {
+            return dg[firstNum] + ' hundred ' + tw[secondNum];
+         }
+         
+         return dg[firstNum] + ' hundred ' + `${tw[secondNum]} ${dg[therdNum]}`;
     }
 };
